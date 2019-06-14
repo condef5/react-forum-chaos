383d042fd33028b4a21306a31972557d63cb6bfe
condef5 <davis.con.fab@gmail.com>
2019-06-14 16:26:36 -0500

    List discussion by date

--- src/components/discussion-list.js
+++ src/components/discussion-list.js
@@ -9,12 +9,14 @@ function DiscussionList({ discussions }) {
       css={{
         listStyle: "none",
         padding: 0,
-        margin: 0
+        margin: "2em 0"
       }}
     >
-      {discussions.map(discussion => (
-        <Discussion discussion={discussion} key={discussion.date} />
-      ))}
+      {discussions
+        .sort((a, b) => new Date(b.date) - new Date(a.date))
+        .map(discussion => (
+          <Discussion discussion={discussion} key={discussion.id} />
+        ))}
     </ul>
   );
 }
