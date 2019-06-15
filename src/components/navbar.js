76b4151fdcb5326a1d4ff97a8ac7b3506e5331cd
Lian Nivin <38519819+liannivin@users.noreply.github.com>
2019-06-14 21:23:10 -0500

    Feature login component (#6)
    * Add boilerplate project (#4)
    
    * Add component with errors
    
    * Add fixed login component
    
    * Advance
    
    * Add logout component with errors
    
    * Fix logout component
    
    * Add tests
    
    * Sincronize ui components
    
    * Fix last commit
    
    * Delete link
    
    * Add test for navbar component

--- src/components/navbar.js
+++ src/components/navbar.js
@@ -3,7 +3,12 @@ import React from "react";
 import { jsx } from "@emotion/core";
 import { Link } from "@reach/router";
 
-function Navbar() {
+function Navbar({ user }) {
+  function deleteSession() {
+    localStorage.removeItem("user");
+    window.location = "/";
+  }
+
   return (
     <nav>
       <a href="/">
@@ -13,12 +18,18 @@ function Navbar() {
         <li>
           <Link to="/">Home</Link>
         </li>
-        <li>
-          <Link to="/login">Login</Link>
-        </li>
-        <li>
-          <Link to="/logout">Logout</Link>
-        </li>
+
+        {user.username ? (
+          <li>
+            <a href="#" onClick={deleteSession}>
+              Logout
+            </a>
+          </li>
+        ) : (
+          <li>
+            <Link to="/login">Login</Link>
+          </li>
+        )}
       </ul>
     </nav>
   );
