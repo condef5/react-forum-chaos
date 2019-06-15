b3b9941957298f07dd20e9c9e882d84fadbe22aa
Lian Nivin <liamrn94@gmail.com>
2019-06-14 20:44:26 -0500

    Fix last commit

--- src/views/login.js
+++ src/views/login.js
@@ -27,42 +27,20 @@ function Login({ createUser }) {
       <Link to="/">Go to Home</Link>
       <Card
         css={{
-          background: "gray",
-          display: "flex",
-          justifyContent: "center",
-          alignItems: "center",
-          height: "100vh",
-          width: "100%"
+          backgroundColor: "white",
+          borderRadius: ".5rem",
+          padding: "2rem 1rem",
+          width: "50%",
+          maxWidth: 360,
+          margin: "auto"
         }}
       >
-        <form
-          css={{
-            backgroundColor: "white",
-            borderRadius: ".5rem",
-            padding: "2rem 1rem",
-            width: "50%",
-            maxWidth: 360
-          }}
-          onSubmit={handleSubmit}
-        >
+        <form onSubmit={handleSubmit}>
           <h1 css={{ margin: 0, fontWeight: "500", textAlign: "center" }}>
             Welcome to Chaos
           </h1>
           <div css={{ margin: "2.5rem 1rem" }}>
-            <input
-              css={{
-                border: "none",
-                borderBottom: "1px solid #ccc",
-                padding: ".5rem 1rem",
-                fontSize: "1.2rem",
-                boxSizing: "border-box",
-                width: "100%",
-                outline: "none",
-                transition: "border-bottom-color 300ms ease",
-                ":focus": {
-                  borderBottomColor: "#2C3A47"
-                }
-              }}
+            <Input
               aria-label="Enter your username"
               placeholder="Enter your username"
               type="text"
@@ -76,19 +54,6 @@ function Login({ createUser }) {
 
           <div css={{ margin: "2.5rem 1rem" }}>
             <Input
-              css={{
-                border: "none",
-                borderBottom: "1px solid #ccc",
-                padding: ".5rem 1rem",
-                fontSize: "1.2rem",
-                boxSizing: "border-box",
-                width: "100%",
-                outline: "none",
-                transition: "border-bottom-color 300ms ease",
-                ":focus": {
-                  borderBottomColor: "#2C3A47"
-                }
-              }}
               aria-label="Enter your email"
               placeholder="Enter your email"
               type="email"
@@ -100,23 +65,7 @@ function Login({ createUser }) {
             />
           </div>
 
-          <Button
-            css={{
-              backgroundColor: "#55E6C1",
-              borderRadius: ".5rem",
-              color: "white",
-              width: "100%",
-              padding: ".75rem .5rem",
-              boxSizing: "border-box",
-              fontSize: ".9rem",
-              fontWeight: "500",
-              textTransform: "uppercase",
-              outline: "none"
-            }}
-            type="submit"
-          >
-            Enter
-          </Button>
+          <Button type="submit">Enter</Button>
         </form>
       </Card>
     </>
